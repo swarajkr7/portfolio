87c370493f8d177f052ff8f640cae9296224cbcd
Swarajkr7 <swaraj853@gmail.com>
2025-01-03 19:35:31 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -318,19 +318,19 @@ export const Bio = {
   export const projects = [
     {
       id: 9,
-      title: "Trackify",
+      title: "SS NEWS",
       date: "Jun 2023 - Jul 2023",
       description:
         "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
       image:
         "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
       tags: [
-        "Docker",
-        "AWS",
-        "DuckDNS",
-        "Eslint",
-        "Husky",
-        "CI/CD",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Node Js",
+        "MongoDB",
+        "Express",
         "React Js",
         "MongoDb",
         "Node Js",
@@ -343,13 +343,13 @@ export const Bio = {
     },
     {
       id: 0,
-      title: "Podstream",
+      title: "Dominos Clone",
       date: "Apr 2023 - May 2023",
       description:
-        "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+        "Domino's is an American Michigan-based multinational pizza restaurant chain",
       image:
         "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-      tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+      tags: ["HTML", "CSS", "JavaScript"],
       category: "web app",
       github: "https://github.com/rishavchanda/Podstream",
       webapp: "https://podstream.netlify.app/",
